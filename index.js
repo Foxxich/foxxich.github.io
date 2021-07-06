08b1bc312434bd199a5ee4f1db3e7be0d627c155
Foxxich <64288356+Foxxich@users.noreply.github.com>
2021-07-06 09:15:39 +0200

    Scss + background

---
+++ index.js
@@ -0,0 +1,9 @@
+/*jslint browser:true */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
