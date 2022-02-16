44a53cfdedc9f8a3ce1cd63f7dadf2f50a572aa6
Foxxich <64288356+Foxxich@users.noreply.github.com>
2022-02-16 23:18:24 +0100

    New image

---
+++ convert.py
@@ -0,0 +1,4 @@
+from PIL import Image
+
+im1 = Image.open(r'C:\\Users\\Vadym\\Documents\\GitHub\\foxxich.github.io\\cv.png')
+im1.save(r'C:\\Users\\Vadym\\Documents\\GitHub\\foxxich.github.io\\cv.jpg')
